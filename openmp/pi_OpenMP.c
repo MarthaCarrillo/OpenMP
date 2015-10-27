e0a9bc89b134331d95e99dd2f044b8bea72d7737
MarthaCarrillo <ingmarthacarrillo@gmail.com>
2015-10-27 11:42:33 -0500

    Update pi_OpenMP.c

--- openmp/pi_OpenMP.c
+++ openmp/pi_OpenMP.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <omp.h>
-#define CHUNKSIZE 100
+#define CHUNKSIZE 1000
 
 static long num_steps = 10000;
 
@@ -13,7 +13,7 @@ void main() {
 	double sum = 0.0;
 	step = 1.0/(double) num_steps;
 	
-	#pragma omp parallel for default(shared) private(i) \
+	#pragma omp parallel for default(shared) private(i, x) \
 	 schedule(static, chunk) reduction(+:sum)
 		for (i = 0; i < num_steps; i++) {
 			x = (i + 0.5)*step;
